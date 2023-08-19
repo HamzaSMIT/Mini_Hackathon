6046f51d375a64482fe2eb9df99468761b37bc12
HamzaSMIT <creatorbro135@gmail.com>
2023-08-19 15:41:23 +0500

    hackathon

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAaG245jmJzs6hhaNlpqqUbeNK_vB0Vc7c",
+    authDomain: "hamza-705b4.firebaseapp.com",
+    projectId: "hamza-705b4",
+    storageBucket: "hamza-705b4.appspot.com",
+    messagingSenderId: "976739194584",
+    appId: "1:976739194584:web:19313480180b3f951e69ad",
+    measurementId: "G-1CNP77B8ES"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
